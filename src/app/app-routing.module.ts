f92fd24383b131f1a698b8fe0d3247bd50f738f9
jecontreras <joseeduar147@gmail.com>
2020-06-04 12:14:40 -0500

    fuction nueva

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UsariosComponent } from './component/usarios/usarios.component';
+import { LuisComponent } from './component/luis/luis.component';
 
 
 const routes: Routes = [
   {
     path: "usuarios", component: UsariosComponent
+  },
+  {
+    path: "luis", component: LuisComponent
   }
 ];
 
--- src/app/component/luis/luis.component.ts
+++ src/app/component/luis/luis.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./luis.component.scss']
 })
 export class LuisComponent implements OnInit {
-
+  listUsuari:any=[
+    {
+      nombre: "luis",
+      apellido:"caravajal",
+      edad:22
+    }
+  ];
+  btnvisable:boolean = false;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  crearusuario(){
+    this.btnvisable = true;
+  }
+
+  ocultarusuario(){
+    this.btnvisable = false;
   }
 
 }
